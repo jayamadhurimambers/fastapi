d45a3ad4a095895a281bc3407b9203d711339f05
Madhuri <madhurihydmba@gmail.com>
2025-08-22 18:24:18 +0530

    jiranum

--- tests/test_async.py
+++ tests/test_async.py
@@ -5,7 +5,7 @@ from asynwithpytest import task
 class TestAsyncTasks:
     @pytest.mark.asyncio
     async def test_task_runs(self, capsys):
-        await task("test", 0)
+        await task("test", 1)
         captured = capsys.readouterr()
         assert "Task test started" in captured.out
         assert "Task test finished after 0 seconds" in captured.out
